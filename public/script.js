93d8f9a1fcea1b15337fd07d693b42ac8e5500c3
Kristof Richard Nagy <knagy3@jaguarlandrover.com>
2021-01-09 20:20:03 +0100

    almost done

--- public/script.js
+++ public/script.js
@@ -1,82 +1,93 @@
-const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
   path: '/peerjs',
   host: '/',
-  port: '443'
-})
+  port: '3030'
+});
+
 let myVideoStream;
-const myVideo = document.createElement('video')
+const myVideo = document.createElement('video');
 myVideo.muted = true;
-const peers = {}
+const peers = {};
+
+// gets access to the user video
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
   myVideoStream = stream;
-  addVideoStream(myVideo, stream)
+  // add my video stream
+  addVideoStream(myVideo, stream);
+  // add new users' video stream via peer
   myPeer.on('call', call => {
     call.answer(stream)
-    const video = document.createElement('video')
+    const video = document.createElement('video');
     call.on('stream', userVideoStream => {
       addVideoStream(video, userVideoStream)
     })
-  })
-
+  });
+  // listen to user-connected
   socket.on('user-connected', userId => {
     connectToNewUser(userId, stream)
-  })
-  // input value
+  });
+  // input value with JQuerry ($)
   let text = $("input");
   // when press enter send message
-  $('html').keydown(function (e) {
+  $('html').keydown((e) => {
     if (e.which == 13 && text.val().length !== 0) {
       socket.emit('message', text.val());
-      text.val('')
+      text.val('');
     }
   });
+
   socket.on("createMessage", message => {
     $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
-    scrollToBottom()
-  })
-})
+    scrollToBottom();
+  });
+});
 
-socket.on('user-disconnected', userId => {
+// listen to user-disconnected
+socket.on('user-disconnected', (userId) => {
   if (peers[userId]) peers[userId].close()
-})
+});
 
+// make the othes able to join into the room vie peer
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id)
-})
+});
 
-function connectToNewUser(userId, stream) {
-  const call = myPeer.call(userId, stream)
-  const video = document.createElement('video')
+// new user connect via peer
+const connectToNewUser = (userId, stream) => {
+  console.log("userd-id: ", userId);
+  // get the call info
+  const call = myPeer.call(userId, stream);
+  // new video element for the new user
+  const video = document.createElement('video');
+  // joining to the call
   call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
+    addVideoStream(video, userVideoStream);
   })
+  // closing the call
   call.on('close', () => {
-    video.remove()
+    video.remove();
   })
 
-  peers[userId] = call
-}
+  peers[userId] = call;
+};
 
-function addVideoStream(video, stream) {
-  video.srcObject = stream
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
-    video.play()
+    video.play();
   })
-  videoGrid.append(video)
+  videoGrid.append(video);
 }
-
-
 
 const scrollToBottom = () => {
   var d = $('.main__chat_window');
   d.scrollTop(d.prop("scrollHeight"));
-}
-
+};
 
 const muteUnmute = () => {
   const enabled = myVideoStream.getAudioTracks()[0].enabled;
@@ -87,7 +98,7 @@ const muteUnmute = () => {
     setMuteButton();
     myVideoStream.getAudioTracks()[0].enabled = true;
   }
-}
+};
 
 const playStop = () => {
   console.log('object')
@@ -99,36 +110,36 @@ const playStop = () => {
     setStopVideo()
     myVideoStream.getVideoTracks()[0].enabled = true;
   }
-}
+};
 
 const setMuteButton = () => {
   const html = `
-    <i class="fas fa-microphone"></i>
+    <i class="bi bi-mic-fill"></i>
     <span>Mute</span>
   `
   document.querySelector('.main__mute_button').innerHTML = html;
-}
+};
 
 const setUnmuteButton = () => {
   const html = `
-    <i class="unmute fas fa-microphone-slash"></i>
+    <i class="bi bi-mic-mute-fill"></i>
     <span>Unmute</span>
   `
   document.querySelector('.main__mute_button').innerHTML = html;
-}
+};
 
 const setStopVideo = () => {
   const html = `
-    <i class="fas fa-video"></i>
+    <i class="bi bi-camera-video-fill"></i>
     <span>Stop Video</span>
   `
   document.querySelector('.main__video_button').innerHTML = html;
-}
+};;
 
 const setPlayVideo = () => {
   const html = `
-  <i class="stop fas fa-video-slash"></i>
+  <i class="bi bi-camera-video-off-fill"></i>
     <span>Play Video</span>
   `
   document.querySelector('.main__video_button').innerHTML = html;
-}
+};
--- server.js
+++ server.js
@@ -1,42 +1,49 @@
-const express = require('express')
-const app = express()
-// const cors = require('cors')
-// app.use(cors())
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+// node js express server
+const express = require('express');
+const app = express();
+//
+// const cors = require('cors');
+// app.use(cors());
+// set server app
+const server = require('http').Server(app);
+// it allows to make others camera and mic turned on
+const io = require('socket.io')(server);
+// set up peerjs server
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
   debug: true
 });
-const { v4: uuidV4 } = require('uuid')
-
+// unique ID for joining to the room
+const { v4: uuidV4 } = require('uuid');
+// use peer server
 app.use('/peerjs', peerServer);
-
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+// Set the the embedded js engine
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+  //res.status(200).send("Hello World!");
+  res.redirect(`/${uuidV4()}`);
+});
 
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
+  res.render('room', { roomId: req.params.room });
+});
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
+    socket.join(roomId);
     socket.to(roomId).broadcast.emit('user-connected', userId);
     // messages
     socket.on('message', (message) => {
       //send message to the same room
       io.to(roomId).emit('createMessage', message)
-  }); 
+    }); 
 
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
+    });
+  });
 })
 
-server.listen(process.env.PORT||3030)
+server.listen(3030);
